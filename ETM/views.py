fd15e370eec1ea11ccc367fd9c3552579ef0744d
Nicolas <Nicolas.Laoun@he-arc.ch>
2021-02-22 15:21:47 +0100

    added first view

---
+++ ETM/views.py
@@ -0,0 +1,11 @@
+import os
+
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+from ETM.settings import BASE_DIR
+
+
+def index(request):
+    return render(request, 'index.html', {})
