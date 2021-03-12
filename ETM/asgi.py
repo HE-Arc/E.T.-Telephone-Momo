49631daaa49bfba9a0c7ffcddd030edf4c334774
Nicolas <Nicolas.Laoun@he-arc.ch>
2021-03-12 11:51:40 +0100

    changed another thing

--- ETM/asgi.py
+++ ETM/asgi.py
@@ -9,13 +9,14 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import ETMApp.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ETM.settings')
-
+django.setup()
 #application = get_asgi_application()
 
 application = ProtocolTypeRouter({
