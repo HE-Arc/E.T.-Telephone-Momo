ae9130cabf330ce8626dca7ea93fc903c79cd229
Nicolas <Nicolas.Laoun@he-arc.ch>
2021-03-12 10:59:52 +0100

    fixed code

--- ETM/settings.py
+++ ETM/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 import django
-django.setup()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -178,3 +178,5 @@ LOGGING = {
         'propagate': True,
     },
 }
+
+django.setup()
