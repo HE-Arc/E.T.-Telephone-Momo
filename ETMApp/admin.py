87633302ca4286d2a7ae75b829e06e333776327e
Nicolas <Nicolas.Laoun@he-arc.ch>
2021-04-02 14:56:35 +0200

    temp

--- ETMApp/admin.py
+++ ETMApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from ETMApp.models import *
 # Register your models here.
+
+admin.site.register(Author)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
