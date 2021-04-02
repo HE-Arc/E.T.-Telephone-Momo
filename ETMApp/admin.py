2b2fd6760be946a848b167c76ab86ffa981d5e7e
Nicolas <Nicolas.Laoun@he-arc.ch>
2021-04-02 15:10:54 +0200

    added correction

--- ETMApp/admin.py
+++ ETMApp/admin.py
@@ -2,8 +2,27 @@ from django.contrib import admin
 from ETMApp.models import *
 # Register your models here.
 
-admin.site.register(Author)
 
-@admin.register(Author)
-class AuthorAdmin(admin.ModelAdmin):
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(UserAnonyme)
+class UserAnonymeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(UserLike)
+class UserLikeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(UserGame)
+class UserGameAdmin(admin.ModelAdmin):
     pass
--- ETMApp/views.py
+++ ETMApp/views.py
@@ -47,46 +47,86 @@ def game(request):
 
 
 def try_signup(request):
+    # if request.user.is_authenticated:
+    #     return JsonResponse({'error': True, 'msg': 'already_connected'})
+    # if request.method == 'POST':
+    #     if 'username' in request.POST and 'password' in request.POST:
+    #         username = request.POST['username']
+    #         password = request.POST['password']
+    #         if len(username) >= 3 and len(password) >= 3:
+    #             doesExist = User.objects.filter(username=username).exists()
+    #             if not doesExist:
+    #                 user = User.objects.create_user(username, password=password)
+    #                 # user = authenticate(username=username, password=password)
+    #                 auth_login(request, user)
+    #                 return JsonResponse({'error': False, 'msg': 'signed'})
+    #             else:
+    #                 return JsonResponse({'error': True, 'msg': 'user_already_exist'})
+    #         else:
+    #             return JsonResponse({'error': True, 'msg': 'invalid_data'})
+    #     else:
+    #         return JsonResponse({'error': True, 'msg': 'invalid_post'})
+    # else:
+    #     return JsonResponse({'error': True, 'msg': 'invalid_request'})
     if request.user.is_authenticated:
         return JsonResponse({'error': True, 'msg': 'already_connected'})
-    if request.method == 'POST':
-        if 'username' in request.POST and 'password' in request.POST:
-            username = request.POST['username']
-            password = request.POST['password']
-            if len(username) >= 3 and len(password) >= 3:
-                doesExist = User.objects.filter(username=username).exists()
-                if not doesExist:
-                    user = User.objects.create_user(username, password=password)
-                    # user = authenticate(username=username, password=password)
-                    auth_login(request, user)
-                    return JsonResponse({'error': False, 'msg': 'signed'})
-                else:
-                    return JsonResponse({'error': True, 'msg': 'user_already_exist'})
-            else:
-                return JsonResponse({'error': True, 'msg': 'invalid_data'})
-        else:
-            return JsonResponse({'error': True, 'msg': 'invalid_post'})
-    else:
+
+    if request.method != 'POST':
         return JsonResponse({'error': True, 'msg': 'invalid_request'})
+    
+    if 'username' not in request.POST or 'password' not in request.POST:
+        return JsonResponse({'error': True, 'msg': 'invalid_post'})
+
+    username = request.POST['username']
+    password = request.POST['password']
+
+    if len(username) < 3 or len(password) < 3:
+        return JsonResponse({'error': True, 'msg': 'invalid_data'})
+    
+    doesExist = User.objects.filter(username=username).exists()
+
+    if doesExist:
+        return JsonResponse({'error': True, 'msg': 'user_already_exist'})
+
+    user = User.objects.create_user(username, password=password)
+    # user = authenticate(username=username, password=password)
+    auth_login(request, user)
+    return JsonResponse({'error': False, 'msg': 'signed'})
 
 
 def try_login(request):
     if request.user.is_authenticated:
         return JsonResponse({'error': True, 'msg': 'already_connected'})
-    if request.method == 'POST':
-        if 'username' in request.POST and 'password' in request.POST:
-            username = request.POST['username']
-            password = request.POST['password']
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                auth_login(request, user)
-                return JsonResponse({'error': False, 'msg': 'connected'})
-            else:
-                return JsonResponse({'error': True, 'msg': 'invalid_credentials'})
-        else:
-            return JsonResponse({'error': True, 'msg': 'invalid_post'})
-    else:
+    if request.method != 'POST':
         return JsonResponse({'error': True, 'msg': 'invalid_request'})
+    if 'username' not in request.POST or 'password' not in request.POST:
+        return JsonResponse({'error': True, 'msg': 'invalid_post'})
+        
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        auth_login(request, user)
+        return JsonResponse({'error': False, 'msg': 'connected'})
+    return JsonResponse({'error': True, 'msg': 'invalid_credentials'})
+
+
+    # if request.user.is_authenticated:
+    #     return JsonResponse({'error': True, 'msg': 'already_connected'})
+    # if request.method == 'POST':
+    #     if 'username' in request.POST and 'password' in request.POST:
+    #         username = request.POST['username']
+    #         password = request.POST['password']
+    #         user = authenticate(username=username, password=password)
+    #         if user is not None:
+    #             auth_login(request, user)
+    #             return JsonResponse({'error': False, 'msg': 'connected'})
+    #         else:
+    #             return JsonResponse({'error': True, 'msg': 'invalid_credentials'})
+    #     else:
+    #         return JsonResponse({'error': True, 'msg': 'invalid_post'})
+    # else:
+    #     return JsonResponse({'error': True, 'msg': 'invalid_request'})
 
 
 def disconnect(request):
