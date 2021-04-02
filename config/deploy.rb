0a4163ccab072feb962e8f39d1af225ee36d051b
Nicolas <Nicolas.Laoun@he-arc.ch>
2021-04-02 17:41:09 +0200

    test 8

--- config/deploy.rb
+++ config/deploy.rb
@@ -74,7 +74,7 @@ namespace :python do
         execute "source #{venv_path}/bin/activate"
 	    execute "#{venv_path}/bin/pip install -r #{release_path}/requirements.txt"
         execute "#{venv_path}/bin/python3 #{release_path}/manage.py collectstatic --noinput"
-        #execute "#{venv_path}/bin/python3 #{release_path}/manage.py makemigrations --noinput"
+        execute "#{venv_path}/bin/python3 #{release_path}/manage.py makemigrations --noinput"
         execute "#{venv_path}/bin/python3 #{release_path}/manage.py migrate"
 
         end
