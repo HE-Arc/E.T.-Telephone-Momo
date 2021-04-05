82f61d509bc34eb12424fd87e48ed61860315686
mathias.marty <mathias.marty@he-arc.ch>
2021-04-05 21:02:47 +0200

    Use asynchronous request for login

---
+++ ETMApp/static/ETMApp/js/login.js
@@ -0,0 +1,22 @@
+// Bind the input button to the function bellow
+let formDiv = document.getElementById('loginForm');
+formDiv.addEventListener('submit', (e) => {
+    e.preventDefault();
+    sendConnectionRequest();
+});
+
+
+function sendConnectionRequest() {
+    let form = new FormData(formDiv);
+    fetch("/tryLogin", {
+        method: "POST",
+        body: form
+    }).then(response => response.json())
+    .then(data => {
+        // If connection succed
+        if(!data.error)
+            window.location.href = '/';
+        else
+            document.getElementById('errorMessage').removeAttribute('hidden');
+    });
+  }
