0ada83ef4bc4a47d800ec426864cc583b34f0365
Nicolas <Nicolas.Laoun@he-arc.ch>
2021-04-16 16:56:15 +0200

    deploy unfix

--- config/deploy.rb
+++ config/deploy.rb
@@ -73,7 +73,7 @@ namespace :python do
 	    execute "#{venv_path}/bin/pip install -r #{release_path}/requirements.txt"
         execute "#{venv_path}/bin/python3 #{release_path}/manage.py collectstatic --noinput"
         execute "#{venv_path}/bin/python3 #{release_path}/manage.py makemigrations --noinput"
-        execute "#{venv_path}/bin/python3 #{release_path}/manage.py migrate --fake"
+        execute "#{venv_path}/bin/python3 #{release_path}/manage.py migrate"
 
         end
     end
